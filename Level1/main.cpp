521107c86e1f4083d871cbcb134e6bdf04811eac
JoyLeeSoft <jhlcooky@gmail.com>
2014-07-28 11:38:50 +0400

    Level1 commit!

---
+++ Level1/main.cpp
@@ -0,0 +1,18 @@
+/*
+ * Level1 : Message box
+ * Author : Lee <jhlcooky@gmail.com>, Kim<vsp123@naver.com>
+ */
+
+#include <windows.h>
+
+int main()
+{
+	HWND hWnd = GetConsoleWindow();
+	if (hWnd)
+		ShowWindow(hWnd, SW_HIDE);
+
+	while (true)
+	{
+		MessageBoxW(NULL, L"Welcome to the VirusCollection!\nGood luck!", L"Level1", MB_OK);
+	}
+}
