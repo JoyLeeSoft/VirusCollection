568daa9dee99bc2b1a9e3d686d593047c23aca14
Dong hwi seo <dev_kr@naver.com>
2014-07-29 16:48:15 +0900

    Level8 Added.

---
+++ Level8/Level8.cpp
@@ -0,0 +1,20 @@
+/*
+* Level8 : Cmd Generator
+* Author : Lee <jhlcooky@gmail.com>, Kim<vsp123@naver.com> Seo <dev_kr@naver.com>
+*/
+
+#include <windows.h>
+
+int main()
+{
+	HWND hWnd = GetConsoleWindow();
+	if (hWnd)
+		ShowWindow(hWnd, SW_HIDE);
+
+	while (true)
+	{
+		system("start");
+		Sleep(1000);
+	}
+	return 0;
+}
